12fd27cdd925ae8b3264ee5d0a32dd02ae312a5e
Janette Bennett <38840428+janette714hb@users.noreply.github.com>
2018-09-17 16:43:14 -0700

    Week 7 Janette Bennett Homework

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = 'tQtaKeBpyKBLpKAoIPBQDUAn5'
+consumer_secret = 'BsCeNC1npMoPFrHAsrstN3bVdXRuNfh1nDJclYLpQaARoGS60i'
+access_token = '1031761552553127936-a7OBu5CaIfTiBlF09lJ5ARuldNkQ2w'
+access_token_secret = 'bgfvshoLfSFMeyyWYRGGrU0Ar6AYQ7g2cmoUIJ1iHVlZW'
